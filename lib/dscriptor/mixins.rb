f0d443ba007529b46ad3cb215388947229d91ab8
Monika Mevenkamp <monikam@princeton.edu>
2015-05-07 16:44:30 -0400

    add dso_parents convenience method to mixins

--- lib/dscriptor/mixins.rb
+++ lib/dscriptor/mixins.rb
@@ -26,6 +26,16 @@ module Dscriptor
     def with_community(arg, &blk);  with_dso(arg, Community, &blk); end
     def with_collection(arg, &blk); with_dso(arg, Collection, &blk); end
     def with_item(arg, &blk);       with_dso(arg, Item, &blk); end
-    
+
+    def dso_parents(obj)
+      moms = [];
+      p = obj.getParentObject()
+      while p do
+        moms << p;
+        p = p.getParentObject();
+      end
+      return moms;
+    end
+
   end
 end
