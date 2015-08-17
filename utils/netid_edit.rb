58d0d32cd8ee9b1d64b1a085d10b831e038fdf01
Monika Mevenkamp <monikam@princeton.edu>
2015-08-17 17:39:31 -0400

    edit command for netid - aka eperson account

---
+++ utils/netid_edit.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env jruby -I lib
+require "highline/import"
+require 'dspace'
+DSpace.load
+
+
+def print_members(p)
+  puts "#{p} is member of:"
+  DEPerson.groups(p).each do |g|
+    puts "\t" + g.getName();
+  end
+  puts "";
+end
+
+netid = ask "enter netid "
+p = DEPerson.find(netid);
+raise "no such eperson" if p.nil?
+print_members(p);
+
+yes = ask "want to add to same group as another user ? [Y/N] "
+if (yes == "Y") then
+  who = ask "who ? [enter netid] "
+  o = DEPerson.find(who);
+  raise "no such eperson" if o.nil?
+  print_members(o);
+  pgroups = DEPerson.groups(p)
+  DEPerson.groups(o).each do |g|
+    if (pgroups.select { |pg| pg.getName() == g.getName }.empty?) then
+      yes = ask "add #{p} to #{g.getName()} ? [Y/N] ";
+      if (yes == "Y") then
+        puts "\tadding #{p} to #{g}"
+        g.addMember(p);
+        g.update();
+      end
+    else
+      puts "\talready member of #{g}"
+    end
+  end
+end
+puts "";
+
+
+yes = ask "want to remove from groups ? [Y/N] "
+if (yes == "Y") then
+  DEPerson.groups(p).each do |g|
+    yes = ask "remove #{p} from #{g.getName()} ? [Y/N] ";
+    if (yes == "Y") then
+      puts "\tremoving from #{g}"
+      g.removeMember(p);
+      g.update();
+    end
+  end
+end
+puts "";
+
+print_members(p);
+yes = ask "want to commit the changes ? [Y/N] "
+if (yes == "Y") then
+  DSpace.commit
+end
