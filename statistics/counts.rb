c33042f664ef44a656fda10023bd1ee51d0854ea
Monika Mevenkamp <monikam@princeton.edu>
2015-08-26 15:10:24 -0400

    count items in collections

---
+++ statistics/counts.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env jruby -I lib
+
+require 'dspace'
+
+DSpace.load
+
+
+comms = DCommunity.all;
+comms.each do |cm|
+   colls = cm.get_all_collections
+   colls.each do |c|
+     count = 0;
+     items = c.getAllItems;
+     while (items.hasNext) do
+       items.nextID
+       count = count + 1;
+     end
+     puts  "#{c.getHandle}\t#{c.getParentObject().getHandle()}\t#{count}\t#{c.getName()}"
+   end
+end
