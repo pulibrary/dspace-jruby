e67db76741bd461450783021019d1b22bd7eecb4
Monika Mevenkamp <monikam@princeton.edu>
2015-09-09 12:16:09 +0200

    stub to create items

--- utils/genTestData.rb
+++ utils/genTestData.rb
@@ -1,8 +1,34 @@
 #!/usr/bin/env jruby  -I lib -I utils
 require 'yaml';
+require 'faker';
 
 test_data_file = "testdata.yml";
-test_data = YAML.load_file(test_data_file);
+
+def generate(file)
+  test_data = YAML.load_file(file)
+  test_data["communities"].each do |comm|
+    parent_name = comm["name"];
+    if parent_name then
+      puts "zap_community #{parent_name}"
+      zap_community(parent_name)
+      parent = create_community(parent_name)
+      puts "created #{parent.getHandle}\t#{parent.getName}"
+      collections = comm["collections"];
+      if (collections) then
+        collections.each do |col|
+          coll = create_collections(parent, col["name"])
+          puts "created in #{parent.getName}\t#{coll.getHandle}\t#{coll.getName}\t"
+          n = col["nitems"] || 0
+          n.times do
+            item = fake_item(coll)
+            puts "created in #{parent.getName}\t#{coll.getHandle}\t#{coll.getName}\t#{item}"
+          end
+          puts "";
+        end
+      end
+    end
+  end
+end
 
 def zap_community(comm_name)
   DCommunity.findAll(comm_name).each do |c|
@@ -19,12 +45,15 @@ def create_community(comm_name)
 end
 
 def create_collections(parent, name)
-  puts "Creating Collections #{name}\tin\t#{parent.getName}";
   new_col = Collection.create(DSpace.context)
   new_col.setMetadata("name", name)
   new_col.update
   parent.addCollection(new_col)
-  puts "Created #{new_col.getHandle()}\t#{new_col.getName}"
+  return new_col;
+end
+
+def fake_item(col)
+  return "item"
 end
 
 
@@ -32,26 +61,13 @@ require 'dspace'
 DSpace.load()
 java_import org.dspace.content.Collection
 java_import org.dspace.content.Community
+java_import org.dspace.content.Item
 
 admin_user = DGroup.find(DGroup::ADMIN_ID).getMembers()[0]
 puts "Using user account: #{admin_user.getEmail()}"
 DSpace.context.setCurrentUser(admin_user)
 
-
-test_data["communities"].each do |comm|
-  parent_name = comm["name"];
-  if parent_name then
-    zap_community(parent_name)
-    parent = create_community(parent_name)
-    collections = comm["collections"];
-    if (collections) then
-      collections.each do |col|
-        create_collections(parent, col["name"])
-      end
-    end
-  end
-end
-
+generate(test_data_file)
 DSpace.commit
 
 
