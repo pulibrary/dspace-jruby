487cc2a827ce10d978b3b7289bb6a71deb8c9773
Monika Mevenkamp <monikam@princeton.edu>
2015-09-14 02:08:39 +0200

    include login_token if available

--- ruby/test_rest_api/lib/dspace_rest.rb
+++ ruby/test_rest_api/lib/dspace_rest.rb
@@ -10,23 +10,33 @@ class DSpaceRest
   end
 
   def login(email, pwd)
-    res = post("/login", {'email' => email, 'password' => pwd})
-    raise "can''t login to #{@baseurl}" if  not res.is_a?(Net::HTTPSuccess)
-    # @login_token = res.body;
+    uri = @baseurl + "/login"
+    params =  {'email' => email, 'password' => pwd}
+    @login_token = RestClient.post  uri, params.to_json, :content_type => :json, :accept => :json
+    return @login_token
+  end
+
+  def logout
+    @login_token = nil;
   end
 
   def get(path, params)
     #TODO send login token along
-    uri = @baseurl + "/" + path;
-    res = RestClient.get uri, { "params" => params, "accept" => :json}
+    options = { "params" => params, :content_type => :json, :accept => :json}
+    options['rest-dspace-token'] = @login_token if (not @login_token.nil?)
+    uri = @baseurl + "/" + path
+    res = RestClient.get uri, options
     @last_res = JSON.parse(res)
     return @last_res
   end
 
   def post(path, params)
     uri = @baseurl + "/" + path;
-    @last_res = RestClient.post  uri, params.to_json, :content_type => :json, :accept => :json
-    @last_res = JSON.parse(res)
+    puts uri;
+    options = { :content_type => :json, :accept => :json}
+    options['rest-dspace-token'] = @login_token if (not @login_token.nil?)
+    @last_res = RestClient.post  uri, params.to_json, options
+    @last_res = JSON.parse(@last_res)
     return @last_res
   end
 
