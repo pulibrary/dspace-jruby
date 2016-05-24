e59b94fc4f76123de29c835c773ea3a0636c394a
Monika Mevenkamp <monikam@princeton.edu>
2016-05-24 11:45:08 -0400

    tweak DWorkflowItem.findAll

--- lib/dspace/dworkflowitem.rb
+++ lib/dspace/dworkflowitem.rb
@@ -4,15 +4,23 @@ require 'dspace'
 class DWorkflowItem
   def self.findAll(obj)
     java_import org.dspace.workflow.WorkflowItem
+    if (obj.nil?) then
+      return WorkflowItem.findAll(DSpace.context)
+    end
     if (obj.getType == DSpace::COLLECTION)
       return WorkflowItem.findByCollection(DSpace.context, obj)
-    elsif   (obj.getType == DSpace::COMMUNITY) then
+    elsif (obj.getType == DSpace::COMMUNITY) then
       flows = []
-      obj.getCollections.each do |col|
+      obj.getAllCollections.each do |col|
         flows += findAll(col)
       end
       return flows
+    elsif (obj.getType == DSpace::ITEM) then
+      wi = WorkflowItem.findByItem(DSpace.context, obj)
+      # to be consistent return array with the unqiue wokflow item
+      return [wi] if wi
     end
+    # return empty array if no matching workflow items
     return []
   end
 
