51fd63df541519c6cf920d39b6bd7605ff12a2c9
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-05-29 11:33:20 -0400

    Create DBitstream framework

---
+++ spec/lib/dspace/dbitstream_spec.rb
@@ -0,0 +1,21 @@
+###
+# This class wraps an org.dspace.content.Bitstream object
+describe DBitstream do
+  
+  describe '.all' do
+  ##
+  # Collect all Bitstream objects from Dspace context
+  # 
+  # @return [Array<org.dspace.content.Bitstream>]
+  end
+
+  describe '.find' do
+  ##
+  # Get corresponding Bitstream object from a given id
+  #
+  # @param id [Integer] the Bitstream id
+  # @return [nil, org.dspace.content.Bitstream] either the corresponding 
+  #   bitstream object or nil if it couldn't be found.
+  end
+
+end
