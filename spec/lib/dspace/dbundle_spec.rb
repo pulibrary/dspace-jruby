dd4d0ecfade03c068ed177994c2ae55d74d356e1
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-05-29 11:35:34 -0400

    Create DBundle framework

---
+++ spec/lib/dspace/dbundle_spec.rb
@@ -0,0 +1,10 @@
+describe DBundle
+  describe '.find'
+  ##
+  # Get corresponding Bundle object from a given id
+  #
+  # @param id [Integer] the Bundle id
+  # @return [nil, org.dspace.content.Bundle] either the corresponding bundle 
+  #   object or nil if it couldn't be found.
+  end
+end
